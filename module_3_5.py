66f685f936c0aee53f66901e13b074e041f582e3
Grikh-UU-Py <5186415@mail.ru>
2024-10-11 01:40:47 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,12 @@
+# Рекурсивное умножение цифр
+def get_multiplied_digits(number):
+    str_number = str(number)
+    if len(str_number) > 1:
+        first = int(str_number[:1])
+        multiplied = first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return number
+    return multiplied
+
+result = get_multiplied_digits(40203)
+print(result)
